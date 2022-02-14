8a394ce410b48130450976c34aca0e4c21cd9316
Amanda-Choi <96817093+Amanda-Choi@users.noreply.github.com>
2022-02-14 07:30:41 -0800

    Added open source link

--- src/App.js
+++ src/App.js
@@ -7,6 +7,15 @@ function App() {
       <header className="App-header">
         <h1>React Weather App</h1>
         <Weather />
+        <div class="source">
+          <a
+            href="https://github.com/Amanda-Choi/Amanda-Weather-App-Project"
+            target="_blank"
+          >
+            Open-source code
+          </a>
+          by Amanda Choi
+        </div>
       </header>
     </div>
   );
